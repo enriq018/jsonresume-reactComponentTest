96bbd34df92963ebbeb34952bb84a47890df3de6
enriq018 <enriq018coding@gmail.com>
2020-03-30 01:18:52 -0700

    add theme request

--- src/App.js
+++ src/App.js
@@ -1,129 +1,72 @@
 import React from 'react';
-import DefaultResume from '@tjscollins/jsonresume-react-components';
+// import DefaultResume from '@tjscollins/jsonresume-react-components';
+import axios from 'axios';
 import './App.css';
 
 // console.log(DefaultResume)
-var mockData ={
-  "basics": {
-    "email": "john@gmail.com",
-    "label": "Programmer",
-    "location": {
-      "address": "2712 Broadway St",
-      "postalCode": "CA 94115",
-      "city": "San Francisco",
-      "countryCode": "US",
-      "region": "California"
-    },
-    "name": "John Doe",
-    "phone": "(912) 555-4321",
-    "picture": "",
-    "profiles": [{
-      "network": "Twitter",
-      "username": "john",
-      "url": "http://twitter.com/john"
-    }],
-    "summary": "A summary of John Doe...",
-    "website": "http://johndoe.com",
-  },
-  "work": [{
-    "company": "Company",
-    "website": "http://company.com",
-    "position": "President",
-    "startDate": "2013-01-01",
-    "endDate": "2014-01-01",
-    "summary": "Description...",
-    "highlights": [
-      "Started the company"
-    ]
-  }],
-  "volunteer": [{
-    "organization": "Organization",
-    "position": "Volunteer",
-    "website": "http://organization.com/",
-    "startDate": "2012-01-01",
-    "endDate": "2013-01-01",
-    "summary": "Description...",
-    "highlights": [
-      "Awarded 'Volunteer of the Month'"
-    ]
-  }],
-  "education": [{
-    "institution": "University",
-    "area": "Software Development",
-    "studyType": "Bachelor",
-    "startDate": "2011-01-01",
-    "endDate": "2013-01-01",
-    "gpa": "4.0",
-    "courses": [
-      "DB1101 - Basic SQL"
-    ]
-  }],
-  "awards": [{
-    "title": "Award",
-    "date": "2014-11-01",
-    "awarder": "Company",
-    "summary": "There is no spoon."
-  }],
-  "publications": [{
-    "name": "Publication",
-    "publisher": "Company",
-    "releaseDate": "2014-10-01",
-    "website": "http://publication.com",
-    "summary": "Description..."
-  }],
-  "skills": [{
-    "name": "Web Development",
-    "level": "Master",
-    "keywords": [
-      "HTML",
-      "CSS",
-      "Javascript"
-    ]
-  }],
-  "languages": [{
-    "language": "English",
-    "fluency": "Native speaker"
-  }],
-  "interests": [{
-    "name": "Wildlife",
-    "keywords": [
-      "Ferrets",
-      "Unicorns"
-    ]
-  }],
-  "references": [{
-    "name": "Jane Doe",
-    "reference": "Reference..."
-  }], 
-  "projects": [
-    "title",
-    "category",
-    "role",
-    "url",
-    "startDate",
-    "endDate",
-    "releaseDate",
-    "repo",
-    "summary",
-    "highlights",
-    "keywords"
-  ]
+import mockData from './mockData.js';
+import mockHtml from './mockHtml.js';
+const themes = ['Elegant', 'Paper', 'Kendall', 'Flat', 'Modern', 'Classy', 'Class', 'Short', 'Kwan', 'OnePage', 'Spartan', 'Stackoverflow']
+function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 
 // console.log(DefaultResume({resumeData:mockData}))
 
-function App() {
-  return (
-      <div className="resume-main-container">
+// function App() {
+//   return (
+      // <div className="resume-main-container">
+      //   <div className="resume-left-container">
+      //     Resume Builder Goes here
+      //   </div>
+      //   <div className='resume-right-container'>
+      //    <DefaultResume resumeData={mockData} />
+      //   </div>
+      // </div>
+//   );
+// }
+// const html = 
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      body: '',
+    }
+  }
+
+  componentDidMount() {
+    const context = this;
+    const theme = themes[getRandomArbitrary(0, themes.length)] 
+    console.log(theme)
+    axios.post(`https://themes.jsonresume.org/theme/` + theme, {
+      resume: mockData
+      })
+      .then(function (response) {
+        // console.log(response.data)
+        context.setState({body: response.data})
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+
+  render() {
+    let context = this;
+    // console.log(context.state.body)
+    return (
+
+
+     <div className="resume-main-container">
         <div className="resume-left-container">
-          Resume Builder Goes here
         </div>
         <div className='resume-right-container'>
-         <DefaultResume resumeData={mockData} />
+          <div dangerouslySetInnerHTML={{__html: context.state.body}} />
         </div>
       </div>
-  );
+    )
+  }
 }
 
 export default App;

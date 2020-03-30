d4cad8c9ccc5f6f2cd14ce4eccfcc16c3d80dd31
enriq018 <enriq018coding@gmail.com>
2020-03-29 22:22:21 -0700

    figure out css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
 import React from 'react';
-import logo from './logo.svg';
+import DefaultResume from '@tjscollins/jsonresume-react-components';
 import './App.css';
+
+// console.log(DefaultResume)
+var mockData ={
+  "basics": {
+    "email": "john@gmail.com",
+    "label": "Programmer",
+    "location": {
+      "address": "2712 Broadway St",
+      "postalCode": "CA 94115",
+      "city": "San Francisco",
+      "countryCode": "US",
+      "region": "California"
+    },
+    "name": "John Doe",
+    "phone": "(912) 555-4321",
+    "picture": "",
+    "profiles": [{
+      "network": "Twitter",
+      "username": "john",
+      "url": "http://twitter.com/john"
+    }],
+    "summary": "A summary of John Doe...",
+    "website": "http://johndoe.com",
+  },
+  "work": [{
+    "company": "Company",
+    "website": "http://company.com",
+    "position": "President",
+    "startDate": "2013-01-01",
+    "endDate": "2014-01-01",
+    "summary": "Description...",
+    "highlights": [
+      "Started the company"
+    ]
+  }],
+  "volunteer": [{
+    "organization": "Organization",
+    "position": "Volunteer",
+    "website": "http://organization.com/",
+    "startDate": "2012-01-01",
+    "endDate": "2013-01-01",
+    "summary": "Description...",
+    "highlights": [
+      "Awarded 'Volunteer of the Month'"
+    ]
+  }],
+  "education": [{
+    "institution": "University",
+    "area": "Software Development",
+    "studyType": "Bachelor",
+    "startDate": "2011-01-01",
+    "endDate": "2013-01-01",
+    "gpa": "4.0",
+    "courses": [
+      "DB1101 - Basic SQL"
+    ]
+  }],
+  "awards": [{
+    "title": "Award",
+    "date": "2014-11-01",
+    "awarder": "Company",
+    "summary": "There is no spoon."
+  }],
+  "publications": [{
+    "name": "Publication",
+    "publisher": "Company",
+    "releaseDate": "2014-10-01",
+    "website": "http://publication.com",
+    "summary": "Description..."
+  }],
+  "skills": [{
+    "name": "Web Development",
+    "level": "Master",
+    "keywords": [
+      "HTML",
+      "CSS",
+      "Javascript"
+    ]
+  }],
+  "languages": [{
+    "language": "English",
+    "fluency": "Native speaker"
+  }],
+  "interests": [{
+    "name": "Wildlife",
+    "keywords": [
+      "Ferrets",
+      "Unicorns"
+    ]
+  }],
+  "references": [{
+    "name": "Jane Doe",
+    "reference": "Reference..."
+  }], 
+  "projects": [
+    "title",
+    "category",
+    "role",
+    "url",
+    "startDate",
+    "endDate",
+    "releaseDate",
+    "repo",
+    "summary",
+    "highlights",
+    "keywords"
+  ]
+}
+
+
+// console.log(DefaultResume({resumeData:mockData}))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="resume-main-container">
+        <div className="resume-left-container">
+          Resume Builder Goes here
+        </div>
+        <div className='resume-right-container'>
+         <DefaultResume resumeData={mockData} />
+        </div>
+      </div>
   );
 }
 

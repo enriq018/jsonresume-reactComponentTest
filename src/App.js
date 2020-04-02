173b12151a815005f3c9396a430b0f5bd6bf39ff
enriq018 <enriq018coding@gmail.com>
2020-04-01 17:13:00 -0700

    basic react setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,311 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './App.css';
+import axios from 'axios';
+import htmlToImage from 'html-to-image';
+
+import paper from './paper.jpeg';
+
+import mockData from './mockData.js';
+// import mockHtml from './mockHtml.js';
+const themes = ['Elegant', 'Paper', 'Kendall', 'Flat', 'Modern', 'Classy', 'Class', 'Short', 'Kwan', 'OnePage', 'Spartan', 'Stackoverflow']
+const getRandomArbitrary = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+const Nav = () => (
+  <div className="collapse bg-dark resume-home-landing-navbar-dropdown" id="navbarHeader">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-7 py-4">
+          <h4  className="text-white">About</h4>
+          <p className="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+      </div>
     </div>
-  );
+  </div>
+)
+const LandingMain = () => (
+  <div className="text-center py-5 resume-home-landing-container" >
+    <div className="container">
+      <div className="row my-5 justify-content-center">
+        <div className="col-md-9">
+          <h1 className="resume-home-landing-title font-weight-bold">Welcome to our Resume Builder!</h1>
+          <p className="lead text-white">You can create a resume using one of our templates.</p>
+          <a href="#" className="btn btn-primary m-2 shadow">Create Resume</a>
+          <a href="#" className="btn btn-primary m-2 shadow">View Saved Resumes</a>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+const DemoContainer = () => (
+  <div className="py-4 bg-light">
+    <div className="container">
+      <div className="row">
+        <DemoTemplate />
+        <DemoTemplate />
+        <DemoTemplate />
+        <DemoTemplate />
+        <DemoTemplate />
+        <DemoTemplate />
+      </div>
+    </div>
+  </div>
+)
+
+const TemplateModalItem = () => (
+  <div className="col-md-4 p-3">
+    <img className="card-img-top card-hover" src={paper}/>
+  </div>     
+)
+
+const TemplateModal = () => (
+  <div className="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div className="modal-dialog modal-dialog-centered   mw-100 w-75" role="document">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h5 className="modal-title" id="exampleModalLongTitle">Select A Resume</h5>
+          <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div className="modal-body">
+          <div className="row">
+            <TemplateModalItem/>
+            <TemplateModalItem/>
+            <TemplateModalItem/>
+            <TemplateModalItem/>
+            <TemplateModalItem/>
+            <TemplateModalItem/>
+          </div>                    
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+const TemplateModal2 = () => (
+  <div className="modal fade" id="exampleModalCenter2" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div className="modal-dialog modal-dialog-centered   mw-100 w-75" role="document">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h5 className="modal-title" id="exampleModalLongTitle">Select A Resume2</h5>
+          <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div className="modal-body">  
+          <div className="row">
+            
+
+          </div>                    
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+const FormLeft = () => (
+  <div className="col-md-3 col-xs-12 register-left">
+    <img className="shadow register-selected-image" src={paper} alt=""/>
+    <h4>Selected Template:</h4>
+    <h5>TEMPLATE NAME</h5>
+    <p>You are minutes away from creating your own resume!</p>
+    <input className="shadow" type="submit" name="" value="Change Template"  data-toggle="modal" data-target="#exampleModalCenter"/>
+    <input className="shadow" type="submit" name="" value="Create Resume" data-toggle="modal" data-target="#exampleModalCenter2"/><br/>
+      <TemplateModal/>
+      <TemplateModal2/>
+  </div>
+)
+
+const FormBasic = () => (
+  <div className="col-md-6">
+    <div className="form-group">
+      <h6 className="text-center font-weight-bold">Personal Info</h6>
+      <input type="text" className="form-control" placeholder="Name *" value="" />
+    </div>
+    <div className="form-group">
+      <input type="text" className="form-control" placeholder="Email *" value="" />
+    </div>
+    <div className="form-group">
+      <input type="text" className="form-control" placeholder="Phone # *" value="" />
+    </div>
+    <hr/>
+    
+  </div>
+)
+
+const FormAddress = () => (
+  <div className="col-md-6">
+    <h6 className="text-center font-weight-bold">Address</h6>
+    <div className="form-group">
+      <input type="text" className="form-control" placeholder="Address *" value="" />
+    </div>
+    <div className="form-group">
+      <input type="text" className="form-control"  placeholder="City *" value="" />
+    </div>
+    <div className="form-group">
+      <input type="text" className="form-control" placeholder="ZipCode *" value="" />
+    </div>
+    <div className="form-group">
+      <input type="text" className="form-control" placeholder="State *" value="" />
+    </div>
+    <hr/>
+    <h6 className="text-center font-weight-bold">Education</h6>
+    <div className="form-group">
+      <input type="text" className="form-control" placeholder="Institution *" value="" />
+    </div>
+    <div className="form-group">
+      <input type="text" className="form-control" placeholder="Area *" value="" />
+    </div>
+    <div className="form-group">
+      <input type="text" className="form-control"  placeholder="Study Type (Bachelor) *" value="" />
+    </div>
+    <div className="form-group">
+      <input type="text" className="form-control"  placeholder="Start Date *" value="" />
+    </div>
+    <div className="form-group">
+      <input type="text" className="form-control" placeholder="End Date *" value="" />
+    </div>
+  </div>
+)
+
+const FormFixMeLater = () => (
+  <div className="row register-form-work">
+    <div className="col-md-12">
+      <h6 className="text-center font-weight-bold">Work Experience</h6>
+      <p >Job 1</p>
+      <div className="form-group">
+        <input type="text" className="form-control" placeholder="Company *" value="" />
+      </div>
+      <div className="form-group">
+        <input type="text" className="form-control" placeholder="Position *" value="" />
+      </div>
+      <div className="form-group">
+        <input type="text" className="form-control"  placeholder="Start Date *" value="" />
+      </div>
+      <div className="form-group">
+        <input type="text" className="form-control"  placeholder="End Date *" value="" />
+      </div>
+      <div className="form-group">
+        <input type="text" className="form-control" placeholder="Label *" value="" />
+      </div>
+      <br/>
+      <p >Job 2</p>
+      <div className="form-group">
+        <input type="text" className="form-control" placeholder="Company *" value="" />
+      </div>
+      <div className="form-group">
+        <input type="text" className="form-control" placeholder="Position *" value="" />
+      </div>
+      <div className="form-group">
+        <input type="text" className="form-control"  placeholder="Start Date *" value="" />
+      </div>
+      <div className="form-group">
+        <input type="text" className="form-control"  placeholder="End Date *" value="" />
+      </div>
+      <div className="form-group">
+        <input type="text" className="form-control" placeholder="Label *" value="" />
+      </div>
+    </div>
+  </div>
+    
+)
+
+const FormRight = () => (
+  <div className="col-md-9 col-xs-12 register-right shadow" id="style-5">
+    <div className="tab-content" id="myTabContent">
+      <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+        <h5 className="register-heading">USERNAME's Resume Information</h5>
+        <div className="row register-form">
+          <FormBasic/>
+          <FormAddress/>
+          <FormFixMeLater/>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+const FormContainer = () => (
+  <div className="container register">
+    <div className="row">
+      <FormLeft/>
+      <FormRight/>
+    </div>
+  </div>
+)
+
+
+
+const DemoTemplate = () => (
+  <div className="col-md-4 p-3">
+    <div className="card box-shadow">
+      <img className="card-img-top" src={paper}/>
+      <div className="card-body">
+        <p className="card-text">ClassName Template</p>
+        <div className="d-flex justify-content-between align-items-center">
+          <div className="btn-group">
+            <button type="button" className="btn btn-sm btn-outline-info align-items-center">Use Template</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+const AppPresentation = ({resume}) => (
+  <React.Fragment>
+    <Nav/>
+    <LandingMain />
+    <DemoContainer/>
+    <hr/>
+    <FormContainer/>
+    <hr/>
+    <div className="container">
+      <div dangerouslySetInnerHTML={{__html: resume}} />
+    </div>
+  </React.Fragment>
+  
+
+
+  
+        
+)
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      body: '',
+    }
+  }
+
+  componentDidMount() {
+    const context = this;
+    const theme = themes[getRandomArbitrary(0, themes.length)] 
+    console.log(theme)
+    axios.post(`https://themes.jsonresume.org/theme/${theme}`, {
+      resume: mockData
+      })
+      .then((response) => {
+        console.log(response)
+        
+        // context.setState({body: response.data})
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }
+
+  render() {
+    return (
+      <AppPresentation resume={this.state.body}/>
+    );
+  }
 }
 
 export default App;
---
+++ src/mockData.js
@@ -0,0 +1,109 @@
+var mockData ={
+  "basics": {
+    "email": "john@gmail.com",
+    "label": "Cool guy",
+    "location": {
+      "address": "2712 Broadway St",
+      "postalCode": "CA 94115",
+      "city": "San Francisco",
+      "countryCode": "US",
+      "region": "California"
+    },
+    "name": "John Wick",
+    "phone": "(912) 555-4321",
+    "picture": "",
+    "profiles": [{
+      "network": "Twitter",
+      "username": "john",
+      "url": "http://twitter.com/john"
+    }],
+    "summary": "A summary of John Doe...",
+    "website": "http://johndoe.com",
+  },
+  "work": [{
+    "company": "Company",
+    "website": "http://company.com",
+    "position": "President",
+    "startDate": "2013-01-01",
+    "endDate": "2014-01-01",
+    "summary": "Description...",
+    "highlights": [
+      "Started the company"
+    ]
+  }],
+  "volunteer": [{
+    "organization": "Organization",
+    "position": "Volunteer",
+    "website": "http://organization.com/",
+    "startDate": "2012-01-01",
+    "endDate": "2013-01-01",
+    "summary": "Description...",
+    "highlights": [
+      "Awarded 'Volunteer of the Month'"
+    ]
+  }],
+  "education": [{
+    "institution": "University",
+    "area": "Software Development",
+    "studyType": "Bachelor",
+    "startDate": "2011-01-01",
+    "endDate": "2013-01-01",
+    "gpa": "4.0",
+    "courses": [
+      "DB1101 - Basic SQL"
+    ]
+  }],
+  "awards": [{
+    "title": "Award",
+    "date": "2014-11-01",
+    "awarder": "Company",
+    "summary": "There is no spoon."
+  }],
+  "publications": [{
+    "name": "Publication",
+    "publisher": "Company",
+    "releaseDate": "2014-10-01",
+    "website": "http://publication.com",
+    "summary": "Description..."
+  }],
+  "skills": [{
+    "name": "Web Development",
+    "level": "Master",
+    "keywords": [
+      "HTML",
+      "CSS",
+      "Javascript"
+    ]
+  }],
+  "languages": [{
+    "language": "English",
+    "fluency": "Native speaker"
+  }],
+  "interests": [{
+    "name": "Wildlife",
+    "keywords": [
+      "Ferrets",
+      "Unicorns"
+    ]
+  }],
+  "references": [{
+    "name": "Jane Doe",
+    "reference": "Reference..."
+  }], 
+  "projects": [
+    "title",
+    "category",
+    "role",
+    "url",
+    "startDate",
+    "endDate",
+    "releaseDate",
+    "repo",
+    "summary",
+    "highlights",
+    "keywords"
+  ]
+}
+
+
+module.exports = mockData

998d31faa7786e2649636999b21eacf7865c8835
enriq018 <enriq018coding@gmail.com>
2020-04-09 00:14:17 -0700

    add html css for paper template

--- src/App.js
+++ src/App.js
@@ -3,48 +3,10 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 import mockData from './mockData.js';
-// import mockHtml from './mockHtml.js';
-const themes = ['Elegant', 'Paper', 'Kendall', 'Flat', 'Modern', 'Classy', 'Class', 'Short', 'Kwan', 'OnePage', 'Spartan', 'Stackoverflow']
-const getRandomArbitrary = (min, max) => {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      body: '',
-    }
-  }
-
-  componentDidMount() {
-    const context = this;
-    const theme = themes[getRandomArbitrary(0, themes.length)] 
-    console.log(theme)
-    axios.post(`https://themes.jsonresume.org/theme/${theme}`, {
-      resume: mockData
-      })
-      .then((response) => {
-        // console.log(response.data)
-        context.setState({body: response.data})
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    }
-
-  render() {
-    let context = this;
-    return (
-      <div className="resume-main-container">
-        <div className="resume-left-container">
-        </div>
-        <div className='resume-right-container'>
-          <div dangerouslySetInnerHTML={{__html: context.state.body}} />
-        </div>
-      </div>
-    );
-  }
-}
+import { Resume } from 'react-vitae-material-theme';
+ 
+const App = () => (
+    <Resume resume={mockData} />
+);
 
 export default App;
